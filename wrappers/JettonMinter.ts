540e4ae11e413830ada06d2973fb44901181421b
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-11 18:49:41 +0500

    feat: update wrappers, implement op codes & get methods

--- wrappers/JettonMinter.ts
+++ wrappers/JettonMinter.ts
@@ -1,8 +1,9 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, TupleItemSlice } from '@ton/core';
 
 export type JettonMinterConfig = {
     totalSupply: bigint;
     adminAddress: Address;
+    tgBTCJettonWalletAddress: Address;
     content: Cell;
     jettonWalletCode: Cell;
 };
@@ -11,6 +12,7 @@ export function jettonMinterConfigToCell(config: JettonMinterConfig): Cell {
     return beginCell()
         .storeCoins(config.totalSupply)
         .storeAddress(config.adminAddress)
+        .storeAddress(config.tgBTCJettonWalletAddress)
         .storeRef(config.content)
         .storeRef(config.jettonWalletCode)
         .endCell();
@@ -37,23 +39,70 @@ export class JettonMinter implements Contract {
         });
     }
 
-    async sendMint(provider: ContractProvider, via: Sender, valut: bigint) {
-
+    async sendChangeAdmin(provider: ContractProvider, via: Sender, value: bigint, opts: {
+        queryId: bigint,
+        newAdmin: Address,
+    }) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(3, 32)
+                .storeUint(opts.queryId, 64)
+                .storeAddress(opts.newAdmin)
+                .endCell()
+        })
     }
 
-    async sendChangeAdmin(provider: ContractProvider, via: Sender, valut: bigint) {
-
+    async sendChangeContent(provider: ContractProvider, via: Sender, value: bigint, opts: {
+        queryId: bigint,
+        newContent: Cell,
+    }) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(4, 32)
+                .storeUint(opts.queryId, 64)
+                .storeRef(opts.newContent)
+                .endCell()
+        })
     }
 
-    async sendChangeContent(provider: ContractProvider, via: Sender, valut: bigint) {
+    async sendChangeTGBTCJettonWallet(provider: ContractProvider, via: Sender, value: bigint, opts: {
+        queryId: bigint,
+        newTGBTCJettonWallet: Address,
+    }) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(4, 32)
+                .storeUint(opts.queryId, 64)
+                .storeAddress(opts.newTGBTCJettonWallet)
+                .endCell()
+        })
 
     }
 
     async getJettonData(provider: ContractProvider) {
-
+        let result = await provider.get('get_jetton_data', []);
+        return {
+            totalSupply: result.stack.readBigNumber(),
+            admin: result.stack.readAddress(),
+            content: result.stack.readCell(),
+            jettonWalletCode: result.stack.readCell(),
+        }
     }
 
-    async getWalletAddress(provider: ContractProvider) {
+    async getWalletAddress(provider: ContractProvider, ownerAddress: Address) {
+        let result = await provider.get('get_wallet_address', [
+            {
+                type: "slice",
+                cell: beginCell().storeAddress(ownerAddress).endCell()
+            } as TupleItemSlice
+        ]);
 
+        return result.stack.readAddress();
     }
 }
--- wrappers/JettonWallet.ts
+++ wrappers/JettonWallet.ts
@@ -12,6 +12,8 @@ export function jettonWalletConfigToCell(config: JettonWalletConfig): Cell {
         .storeCoins(config.balance)
         .storeAddress(config.ownerAddress)
         .storeAddress(config.jettonMasterAddress)
+        .storeUint(0, 32)
+        .storeUint(0, 4)
         .storeRef(config.jettonWalletCode)
         .endCell();
 }
@@ -37,15 +39,62 @@ export class JettonWallet implements Contract {
         });
     }
 
-    async sendTransfer(provider: ContractProvider, via: Sender, value: bigint) {
-
+    async sendTransfer(provider: ContractProvider, via: Sender, value: bigint, opts: {
+        queryId: bigint,
+        jettonAmount: bigint,
+        toOwnerAddress: Address,
+        fwdTonAmount: bigint,
+        forwardPayload: Cell | null,
+    }) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(0xf8a7ea5, 32)
+                .storeUint(opts.queryId, 64)
+                .storeCoins(opts.jettonAmount)
+                .storeAddress(opts.toOwnerAddress)
+                .storeAddress(opts.toOwnerAddress)
+                .storeBit(0)
+                .storeCoins(opts.fwdTonAmount)
+                .storeMaybeRef(opts.forwardPayload)
+                .endCell()
+        })
     }
 
-    async sendBurn(provider: ContractProvider, via: Sender, value: bigint) {
-
+    async sendBurn(provider: ContractProvider, via: Sender, value: bigint, opts: {
+        queryId: bigint,
+        amount: bigint,
+        responseAddress: Address,
+    }) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(0x595f07bc, 32)
+                .storeUint(opts.queryId, 64)
+                .storeCoins(opts.amount)
+                .storeAddress(opts.responseAddress)
+                .storeUint(0, 1)
+                .endCell(),
+        })
     }
 
-    async getWalletData(provider: ContractProvider, via: Sender, value: bigint) {
+    async getWalletData(provider: ContractProvider) {
+        let result = await provider.get('get_wallet_data', []);
+        return {
+            balance: result.stack.readBigNumber(),
+            owner: result.stack.readAddress(),
+            jettonMinter: result.stack.readAddress(),
+            jettonWalletCode: result.stack.readCell(),
+        }
+    }
 
+    async getDepositData(provider: ContractProvider) {
+        let result = await provider.get('get_deposit_data', []);
+        return {
+            depositTimestamp: result.stack.readBigNumber(),
+            apy: result.stack.readBigNumber(),
+        }
     }
 }
